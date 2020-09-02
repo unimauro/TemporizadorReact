34b0cfd30ee27d6ea3e5ad7c740cfeaa1d547065
root <unimauro@gmail.com>
2020-09-02 05:46:20 -0500

    Temporizador

--- temp/src/App.js
+++ temp/src/App.js
@@ -2,24 +2,64 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function FormatDate(props) {
+  return <h2>Esta es la hora temporizada {props.date.toLocaleTimeString()}.</h2>;
+}
+
+class Hora extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>La Hora Temporizada!</h1>
+        <FormatDate date={this.state.date} />
+      </div>
+    );
+  }
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+	  <Hora />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Editando <code>src/App.js</code> recargamos reload y vemos que pasa.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/unimauro/TemporizadorReact"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Probando el Temporizador
         </a>
       </header>
-    </div>
+
+
+ </div>
   );
 }
 
