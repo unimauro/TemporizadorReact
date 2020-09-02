04a4e40b3986b3567d6922465d299a957b17dba2
root <unimauro@gmail.com>
2020-09-02 07:24:34 -0500

    Updates

---
+++ precache-manifest.8ce77c4f16438a24318639cf8871a46d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a16d12c04a82b31fa2bb9a4a76436dc2",
+    "url": "/TemporizadorReact/index.html"
+  },
+  {
+    "revision": "af1f12d16fd819c8c837",
+    "url": "/TemporizadorReact/static/css/main.ac460a07.chunk.css"
+  },
+  {
+    "revision": "dc68ff979271951a80ce",
+    "url": "/TemporizadorReact/static/js/2.2e8ce399.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TemporizadorReact/static/js/2.2e8ce399.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af1f12d16fd819c8c837",
+    "url": "/TemporizadorReact/static/js/main.1c412800.chunk.js"
+  },
+  {
+    "revision": "a2b7a747900702af6fb2",
+    "url": "/TemporizadorReact/static/js/runtime-main.a651b4e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TemporizadorReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
